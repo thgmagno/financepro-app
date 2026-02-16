c809302c941051e1d5497b46080fe8140270e8a1
Magno <thiago@t3w.io>
2026-02-15 21:47:17 -0300

    add endSession button

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -12,6 +12,16 @@ import {
 } from "@/components/ui/sidebar"
 import { LogOut, Settings } from "lucide-react"
 import Link from "next/link"
+import { Button } from "../ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -30,14 +40,29 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </SidebarMenuButton>
               </SidebarMenuItem>
               <SidebarMenuItem>
-                <form action={endSession}>
-                  <SidebarMenuButton asChild>
-                    <button type="submit">
-                      <LogOut />
-                      <span>Logout</span>
-                    </button>
-                  </SidebarMenuButton>
-                </form>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>
+                        <LogOut />
+                        <span>Logout</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-md">
+                    <DialogHeader>
+                      <DialogTitle>End session</DialogTitle>
+                      <DialogDescription>
+                        You will need to authenticate again when you return.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                      <form action={endSession}>
+                        <Button type="submit">Confirm</Button>
+                      </form>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
